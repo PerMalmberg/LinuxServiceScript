219675f7ef232dfd1704c6e2c6b9c1d2b930f315
Per Malmberg <per.malmberg@gmail.com>
2016-07-23 16:55:56 +0200

    Added script

---
+++ MyService.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+SERVICE_NAME="MyService"
+PATH_TO_JAR="/usr/local/MyProject/MyJar.jar"
+PATH_TO_JAVA="java"
+PATH_TO_PID="/tmp/$SERVICE_NAME-pid"
+
+StartService() 
+{
+	if [ -f $PATH_TO_JAR ]; then
+		echo "Starting $SERVICE_NAME ..."
+		if [ -f $PATH_TO_PID ]; then
+			echo "$SERVICE_NAME is already running ..."
+		else
+			nohup $PATH_TO_JAVA -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null & echo $! > $PATH_TO_PID
+			echo "$SERVICE_NAME started ..."
+		fi
+	else
+		echo "Specified JAR ($PATH_TO_JAR) does not exist"
+	fi
+}
+
+StopService()
+{
+	if [ -f $PATH_TO_PID ]; then
+		PID=$(cat $PATH_TO_PID);
+		echo "$SERVICE_NAME stopping ..."
+		kill "$PID"
+		echo "$SERVICE_NAME stopped ..."
+		Clean
+	else
+		echo "$SERVICE_NAME is not running ..."
+	fi
+}
+
+Clean()
+{
+	rm "$PATH_TO_PID"
+}
+
+case $1 in
+	start)
+		StartService
+	;;
+	stop)
+		StopService
+	;;
+	restart)
+		StopService
+		StartService
+	;;
+	clean)
+		echo "Removing PID file"
+		Clean
+	;;
+esac
